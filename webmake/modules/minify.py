9598e8dd0638a2e9ec1e9b9bca1f2302858e772a
YellowSharkMT <bowdowntozappa@yahoo.com>
2020-11-12 14:20:20 -0500

    Fixing post-merge issues with Angular annotations functionality.

--- webmake/modules/minify.py
+++ webmake/modules/minify.py
@@ -5,16 +5,21 @@ from . import utils, concat
 def minify_js(input_files, output_file, release=False, annotate_angular=False):
     assert isinstance(input_files, (list, tuple))
 
-    concat.concatenate_input_files(input_files, output_file, release=release)
-
     if not release:
+        concat.concatenate_input_files(input_files, output_file, release=release)
         return
 
     if annotate_angular:
+        # Rather than annotate several individual files (and having to creating temp files as well), it's
+        # easier to just concatenate the input_files right now, and then perform the annotation upon that
+        # one file. We must then update the `input_files` value accordingly, so that it gets passed into
+        # uglify correctly.
+        concat.concatenate_input_files(input_files, output_file, release=release)
+        input_files = [output_file]
         try:
             annotate_angular_injections(output_file, output_file)
         except:
-            # utils.ensure_deleted(output_file)
+            utils.ensure_deleted(output_file)
             raise
 
     if output_file:
@@ -28,7 +33,7 @@ def minify_js(input_files, output_file, release=False, annotate_angular=False):
         output_file,
         '--',
     ]
-    cmdline.extend([output_file])
+    cmdline.extend(input_files)
 
     try:
         utils.run_command(cmdline, 'Failed to minify JS to "{}"'.format(output_file))
@@ -38,7 +43,7 @@ def minify_js(input_files, output_file, release=False, annotate_angular=False):
 
 def annotate_angular_injections(input_file, output_file):
     cmdline = [
-        utils.get_node_bin_dir('ng-annotate'),
+        os.path.join(utils.get_node_bin_path('ng-annotate'), 'ng-annotate.js'),
         '--add',
         '-o',
         output_file,
@@ -46,9 +51,9 @@ def annotate_angular_injections(input_file, output_file):
     cmdline.extend([input_file])
 
     try:
-        utils.run_command(cmdline, 'Failed to annotate Annnngular injections to "{}"'.format(output_file))
+        utils.run_command(cmdline, 'Failed to annotate Angular injections to "{}"'.format(output_file), with_node=True)
     except Exception as e:
-        # utils.ensure_deleted(output_file)
+        utils.ensure_deleted(output_file)
         raise
 
 def minify_css(input_files, output_file, release=False):
