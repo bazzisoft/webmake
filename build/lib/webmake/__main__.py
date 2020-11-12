338af82982c0e2d20e741a9a3ace57ba3d710111
YellowSharkMT <bowdowntozappa@yahoo.com>
2020-11-12 16:52:53 -0500

    Deleting the /build directory, was accidentally committed.

--- build/lib/webmake/__main__.py
+++
@@ -1,2 +0,0 @@
-import webmake.main
-webmake.main.main()
--- build/lib/webmake/cordova/hooks/before_prepare/runwebmake.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-
-cmdline = 'webmk -v'
-if '--release' in os.environ['CORDOVA_CMDLINE']:
-    cmdline += ' --force --release'
-
-print(cmdline)
-os.system(cmdline)
