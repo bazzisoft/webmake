536843bee0aaade0acfea975faa2265360a18aaf
Barak Shohat <barak@bazzisoft.com>
2024-11-20 18:41:25 +0200

    Bump requirement to watchdog>=5.0.0

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "watchdog>=0.8",
+        "watchdog>=5.0.0",
     ],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
